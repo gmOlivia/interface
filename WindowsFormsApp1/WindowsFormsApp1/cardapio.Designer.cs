7833dfb007b39d05f77d201e26e679b668776711
RafaLNunes <akemigabi08@gmail.com>
2025-06-11 11:00:49 -0300

    cardápio

---
+++ WindowsFormsApp1/WindowsFormsApp1/cardapio.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class cardapio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cardápio";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Font = new System.Drawing.Font("EuroRoman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.groupBox1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.groupBox1.Location = new System.Drawing.Point(35, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 151);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 116);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 25);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Bebidas";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(78, 25);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Doces";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 25);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Salgados";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("EuroRoman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(484, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultados para a busca:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Aplicar filtros";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cardapio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "cardapio";
+            this.Text = "cardapio";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/cardapio.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class cardapio : Form
+    {
+        public class produtos
+        {
+            public string nome { get; set; }
+            public string categoria { get; set; }
+            public string preco { get; set; }
+
+            public override string ToString()
+            {
+                return $" {nome} {categoria} - {preco}";
+            }
+        }
+        private List<produtos> Listaprodutos;
+
+        private List<string> categoriaSelecionadas = new List<string>();
+        public cardapio()
+        {
+            InitializeComponent();
+            Listaprodutos = new List<produtos>() {
+
+            new produtos { nome = "Espresso", categoria = "bebida", preco = "4.50" },
+            new produtos { nome = "Americano", categoria = "bebida", preco = "4.00" },
+            new produtos { nome = "Latte", categoria = "bebida", preco = "6.50" },
+            new produtos { nome = "Cappuccino", categoria = "bebida", preco = "8.00" },
+            new produtos { nome = "Mocha", categoria = "bebida", preco = "8.00" },
+            new produtos { nome = "Café gelado", categoria = "bebida", preco = "6.50" },
+            new produtos { nome = "Chás (sabor à escolha)", categoria = "bebida", preco = "5.00" },
+            new produtos { nome = "Chai", categoria = "bebida", preco = "6.50" },
+            new produtos { nome = "Chocolate quente", categoria = "bebida", preco = "8.00" },
+            new produtos { nome = "Matcha latte", categoria = "bebida", preco = "10.00" },
+            new produtos { nome = "Sucos naturais", categoria = "bebida", preco = "9.00" },
+            new produtos { nome = "Macchiato", categoria = "bebida", preco = "5.00" },
+            new produtos { nome = "Água com gás e sem gás", categoria = "bebida", preco = "4.00" },
+            new produtos { nome = "Cookies", categoria = "doce", preco = "10.00" },
+            new produtos { nome = "Brownies", categoria = "doce", preco = "12.00" },
+            new produtos { nome = "Muffin", categoria = "doce", preco = "6.00" },
+            new produtos { nome = "Croissant doce", categoria = "doce", preco = "12.50" },
+            new produtos { nome = "Fatia de bolo", categoria = "doce", preco = "8.00" },
+            new produtos { nome = "Mini cheesecake", categoria = "doce", preco = "12.00" },
+            new produtos { nome = "Pastel de Belém", categoria = "doce", preco = "7.50" },
+            new produtos { nome = "Banana bread", categoria = "doce", preco = "6.50" },
+            new produtos { nome = "Dango", categoria = "doce", preco = "8.50" },
+            new produtos { nome = "Choux", categoria = "doce", preco = "10.00" },
+            new produtos { nome = "Pão de queijo", categoria = "salgado", preco = "6.00" },
+            new produtos { nome = "Croissant salgado", categoria = "salgado", preco = "12.50" },
+            new produtos { nome = "Sanduíches naturais", categoria = "salgado", preco = "8.00" },
+            new produtos { nome = "Wraps", categoria = "salgado", preco = "8.00" },
+            new produtos { nome = "Quiches individuais", categoria = "salgado", preco = "6.00" },
+            new produtos { nome = "Empanadas argentinas", categoria = "salgado", preco = "10.00" },
+            new produtos { nome = "Focaccias individuais", categoria = "salgado", preco = "12.00" },
+            new produtos { nome = "Mini tapiocas recheadas", categoria = "salgado", preco = "4.00" },
+            new produtos { nome = "Porção coxinha", categoria = "salgado", preco = "12.00" },
+        };
+
+        }
+
+            private void checkBox1_CheckedChanged(object sender, EventArgs e)
+            {
+                categoriaSelecionadas.Add("Pão de queijo"); // salgado
+                categoriaSelecionadas.Add("Croissant salgado"); // salgado
+                categoriaSelecionadas.Add("Sanduíches naturais"); // salgado
+                categoriaSelecionadas.Add("Wraps"); // salgado
+                categoriaSelecionadas.Add("Quiches individuais"); // salgado
+                categoriaSelecionadas.Add("Empanadas argentinas"); // salgado
+                categoriaSelecionadas.Add("Focaccias individuais"); // salgado
+                categoriaSelecionadas.Add("Mini tapiocas recheadas"); // salgado
+                categoriaSelecionadas.Add("Porção coxinha"); //salgado
+            }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            categoriaSelecionadas.Add("Cookies"); // doce
+            categoriaSelecionadas.Add("Brownies"); // doce
+            categoriaSelecionadas.Add("Muffin"); // doce
+            categoriaSelecionadas.Add("Croissant doce"); // doce
+            categoriaSelecionadas.Add("Fatia de bolo"); // doce
+            categoriaSelecionadas.Add("Mini cheesecake"); // doce
+            categoriaSelecionadas.Add("Pastel de Belém"); // doce
+            categoriaSelecionadas.Add("Banana bread"); // doce
+            categoriaSelecionadas.Add("Dango"); // doce
+            categoriaSelecionadas.Add("Choux"); // doce
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            categoriaSelecionadas.Add("Espresso"); // bebida
+            categoriaSelecionadas.Add("Americano"); // bebida
+            categoriaSelecionadas.Add("Latte"); // bebida
+            categoriaSelecionadas.Add("Cappuccino"); // bebida
+            categoriaSelecionadas.Add("Mocha"); // bebida
+            categoriaSelecionadas.Add("Café gelado"); // bebida
+            categoriaSelecionadas.Add("Chás (sabor à escolha)"); // bebida
+            categoriaSelecionadas.Add("Chai"); // bebida
+            categoriaSelecionadas.Add("Chocolate quente"); // bebida
+            categoriaSelecionadas.Add("Matcha latte"); // bebida
+            categoriaSelecionadas.Add("Sucos naturais"); // bebida
+            categoriaSelecionadas.Add("Macchiato"); // bebida
+            categoriaSelecionadas.Add("Água com gás e sem gás"); // bebida
+        }
+    }
+    }
+
